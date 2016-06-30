482bc2e23abbec4d01acec6e9055d808a7d6422c
Aaron Lidman <aaronlidman@gmail.com>
2016-06-30 16:33:07 -0700

    Save tile map results to a file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var tr = require('tile-reduce');
 var path = require('path');
+var fs = require('fs');
 
 var total = 0;
 
@@ -8,13 +9,13 @@ if (!process.argv[2]) return console.log('you must specify an mbtiles');
 tr({
     zoom: 12,
     map: path.join(__dirname, '/miles-of-roads.js'),
+    output: fs.createWriteStream('tilemap.ldgeojson'),
     sources: [{
         name: 'osm',
-        mbtiles: process.argv[2],
-        raw: false
+        mbtiles: process.argv[2]
     }]
 }).on('reduce', function(result) {
-    total += result.roads;
+    total += result;
 }).on('end', function() {
     var result = {
         total: total
