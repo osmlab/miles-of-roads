2a5f5e1303199a04211f60cf7f0f6c0cf181fbfb
Aaron Lidman <aaronlidman@gmail.com>
2016-07-03 18:32:15 -0400

    Get automatic scales working

--- index.js
+++ index.js
@@ -3,31 +3,10 @@ var path = require('path');
 var fs = require('fs');
 var scale = require('d3-scale');
 
-var steps = 6;
-
-
 min = 0;
 max = process.argv[2];
 
-// ---
-
-var values = {};
-
-scale = scale.scaleLog()
-    .domain([1, steps])
-    .range([min, max]);
-
-for (var i = steps; i >= 0; i--) {
-    if (i === 0) break;
-    var previousMax = 0;
-    var max = Math.floor(scale(i));
-    values[i] = [previousMax, max];
-}
-
-console.log(JSON.stringify(values, null, 4));
-
-return 
-
+var steps = 6;
 var total = 0;
 var min = 0;
 var max = 0;
@@ -37,6 +16,7 @@ if (!process.argv[2]) return console.log('you must specify an mbtiles');
 tr({
     zoom: 12,
     map: path.join(__dirname, '/miles-of-roads.js'),
+    bbox: [-120.176697, 32.775729, -115.573425, 34.955744],
     output: fs.createWriteStream('tilemap.ldgeojson'),
     sources: [{
         name: 'osm',
@@ -47,21 +27,33 @@ tr({
     if (result < min) min = result;
     total += result;
 }).on('end', function() {
-    var values = {};
-
-    scale = scale.scaleLog()
-        .domain([1, steps])
-        .range([min, max]);
-
-    for (var i = steps; i >= 0; i--) {
-        values[i] = [];
-    };
-
     var result = {
         min: min,
         max: max,
-        total: total
+        total: total,
+        scale: calculateScale(steps, min, max)
     };
 
-    console.log(JSON.stringify(result));
+    console.log(JSON.stringify(result, null, 4));
 });
+
+function calculateScale(steps, min, max) {
+    var values = {};
+
+    scale = scale.scaleLog()
+        .domain([1, (steps+1)])
+        .range([min, max]);
+
+    console.log(scale(2));
+
+    var previousMax = 0;
+    for (var i = 0; i <= steps; i++) {
+        if (i !== 0) {
+            var max = Math.floor(scale(i+1));
+            values[i] = [previousMax, max];
+            previousMax = max;
+        }
+    }
+
+    return values;
+}
